ac303f248a5b94b8824bedb6116c5d952afe17f4
RISHII7 <rishikesh.palande07@gmail.com>
2025-04-24 23:43:35 +0530

    06: Categories Finalization

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -2,6 +2,8 @@ import { getPayload } from "payload";
 import { Category } from "@/payload-types";
 import configPromise from "@payload-config";
 
+import { CustomCategory } from "./types";
+
 import { Navbar } from "./navbar";
 import { Footer } from "./footer";
 import { SearchFilters } from "./search-filters";
@@ -24,9 +26,10 @@ const Layout = async ({ children }: Props) => {
             exists: false
           },
         },
+        sort: "name"
     });
 
-    const formattedData = data.docs.map((doc) => ({
+    const formattedData: CustomCategory[] = data.docs.map((doc) => ({
         ...doc,
         subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
             // Because of 'depth: 1' we are confident "doc" will be a type of "Category"
--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,60 +1,76 @@
-"use client"
+"use client";
 
+import Link from "next/link";
 import { useRef, useState } from "react";
 
 import { cn } from "@/lib/utils";
-import { Category } from "@/payload-types";
 import { useDropdownPosition } from "@/hooks/use-dropdown-position";
 
 import { Button } from "@/components/ui/button";
 
+import { CustomCategory } from "../types";
 import { SubCategoryMenu } from "./subcategory-menu";
 
 interface CategoryDropdownProps {
-    category: Category;
-    isActive?: boolean;
-    isNavigationHovered?: boolean;
-};
+  category: CustomCategory;
+  isActive?: boolean;
+  isNavigationHovered?: boolean;
+}
 
-export const CategoryDropdown = ({ category, isActive, isNavigationHovered }: CategoryDropdownProps) => {
+export const CategoryDropdown = ({
+  category,
+  isActive,
+  isNavigationHovered,
+}: CategoryDropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const { getDropdownPosition } = useDropdownPosition(dropdownRef);
 
   const onMouseEnter = () => {
-    if(category.subcategories) {
+    if (category.subcategories) {
       setIsOpen(true);
-    };
+    }
   };
 
   const onMouseLeave = () => {
-    if(category.subcategories) {
+    if (category.subcategories) {
       setIsOpen(false);
-    };
+    }
   };
 
   const dropdownPosition = getDropdownPosition();
 
+  // TODO: Potentially improve mobile
+  // const toggleDropDown = () => {
+  //   if (category.subcategories?.docs?.length) {
+  //     setIsOpen(!isOpen);
+  //   };
+  // };
+
   return (
-    <div 
+    <div
       ref={dropdownRef}
       className="relative"
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
+      // onClick={toggleDropDown}
     >
       <div className="relative">
-        <Button 
-            variant="elevated"
-            className={cn(
-              "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-              isActive && !isNavigationHovered && "bg-white border-primary"
-              // 2:49:41
-            )}
-            >
+        <Button
+          variant="elevated"
+          className={cn(
+            "h-11 px-6 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+            isActive && !isNavigationHovered && "bg-white border-primary",
+            isOpen &&
+              "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]"
+          )}
+        >
+          <Link href={`/${category.slug === "all" ? "" : category.slug}`}>
             {category.name}
+          </Link>
         </Button>
         {category.subcategories && category.subcategories.length > 0 && (
-          <div 
+          <div
             className={cn(
               "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
               isOpen && "opacity-100"
@@ -62,7 +78,7 @@ export const CategoryDropdown = ({ category, isActive, isNavigationHovered }: Ca
           />
         )}
       </div>
-      <SubCategoryMenu 
+      <SubCategoryMenu
         category={category}
         isOpen={isOpen}
         position={dropdownPosition}
