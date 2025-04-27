04d4dd0b3cf36529e04a43064303d252281d8d6f
RISHII7 <rishikesh.palande07@gmail.com>
2025-04-27 22:58:45 +0530

    09: Auth States

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,7 +1,11 @@
 "use client"
 
+import Link from "next/link";
 import { useState } from "react";
-import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
+
+import { useTRPC } from "@/trpc/client";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -14,6 +18,9 @@ interface SearchInputProps {
 export const SearchInput = ({ disabled }: SearchInputProps) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  const trpc = useTRPC();
+  const session = useQuery(trpc.auth.session.queryOptions());
+
   return (
     <div className="flex items-center gap-2 w-full">
         <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
@@ -21,7 +28,6 @@ export const SearchInput = ({ disabled }: SearchInputProps) => {
             <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
             <Input className="pl-8" placeholder="Search Products" disabled={disabled} />
         </div>
-        {/* TODO: Add categories view all button */}
         <Button 
           variant="elevated"
           className="size-12 shrink-0 flex lg:hidden"
@@ -29,6 +35,18 @@ export const SearchInput = ({ disabled }: SearchInputProps) => {
         >
           <ListFilterIcon />
         </Button>
+        {session.data?.user && (
+          <Button
+            asChild
+            variant="elevated"
+
+          >
+            <Link href="/library">
+              <BookmarkCheckIcon />
+              Library
+            </Link>
+          </Button>
+        )}
         {/* TODO: Add Library button */}
     </div>
   )
