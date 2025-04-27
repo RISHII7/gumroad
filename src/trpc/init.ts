9c0603baf35a12d2527d27ba809a18fa3eb97cbc
RISHII7 <rishikesh.palande07@gmail.com>
2025-04-27 14:29:43 +0530

    Enabling the superjson transformer

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,8 +1,10 @@
 import { cache } from 'react';
+import superjson from "superjson";
 import { getPayload } from 'payload';
-import config from "@/payload.config";
 
 import { initTRPC } from '@trpc/server';
+
+import config from "@/payload.config";
 
 export const createTRPCContext = cache(async () => {
   /**
@@ -20,7 +22,7 @@ const t = initTRPC.create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
-  // transformer: superjson,
+  transformer: superjson,
 });
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
