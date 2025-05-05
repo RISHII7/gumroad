05b49bca51b139c4b8ac45204509a849d2059e87
RISHII7 <rishikesh.palande07@gmail.com>
2025-05-05 21:48:25 +0530

    14: Product List UI

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,32 +1,37 @@
-import { Suspense } from "react";
+import { SearchParams } from "nuqs/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { getQueryClient, trpc } from "@/trpc/server";
 
-import { ProductList } from "@/modules/products/ui/components/product-list";
-import { ProductListSkeleton } from "@/modules/products/ui/components/product-list-skeleton";
+import { loadProductFilters } from "@/modules/products/utils/search-params";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
 
 interface Props {
-    params: Promise<{
-        subcategory: string;
-    }>;
+  params: Promise<{
+    subcategory: string;
+  }>;
+  searchParams: Promise<SearchParams>;
+}
+
+const Page = async ({ params, searchParams }: Props) => {
+  const { subcategory } = await params;
+  const filters = await loadProductFilters(searchParams);
+
+  // console.log(JSON.stringify(filters), "THIS IS FROM RSC")
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.products.getMany.queryOptions({
+      category: subcategory,
+      ...filters,
+    })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductListView category={subcategory} />
+    </HydrationBoundary>
+  );
 };
 
-const Page = async ({ params }: Props) => {
-    const { subcategory } = await params;
-
-    const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
-        category: subcategory
-    }));
-
-    return ( 
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<ProductListSkeleton />}>
-                <ProductList category={subcategory} />
-            </Suspense>
-        </HydrationBoundary>
-     );
-};
- 
 export default Page;
