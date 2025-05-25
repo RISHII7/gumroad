41489c2ab62a3562435ed77d4f0097df8636aa57
RISHII7 <rishikesh.palande07@gmail.com>
2025-05-25 17:47:35 +0530

    Created 'verify' procedure to allow tenants to verify and Added button to verify from CMS

--- src/app/(app)/api/stripe/webhooks/route.ts
+++ src/app/(app)/api/stripe/webhooks/route.ts
@@ -34,7 +34,10 @@ export async function POST(req: Request) {
 
   console.log("✅ Success: ", event.id);
 
-  const permittedEvents: string[] = ["checkout.session.completed"];
+  const permittedEvents: string[] = [
+    "checkout.session.completed",
+    "account.updated"
+  ];
 
   const payload = await getPayload({ config });
 
@@ -87,6 +90,22 @@ export async function POST(req: Request) {
               },
             });
           }
+          break;
+        
+        case "account.updated":
+          data = event.data.object as Stripe.Account;
+
+          await payload.update({
+            collection: "tenants",
+            where: {
+              stripeAccountId: {
+                equals: data.id
+              },
+            },
+            data: {
+              stripeDetailsSubmitted: data.details_submitted,
+            },
+          });
           break;
 
         default:
--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -10,6 +10,50 @@ import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init
 import { CheckoutMetadata, ProductMetadata } from "@/modules/checkout/types/index";
 
 export const checkoutRouter = createTRPCRouter({
+  verify: protectedProcedure
+    .mutation(async ({ ctx }) => {
+      const user = await ctx.db.findByID({
+        collection: "users",
+        id: ctx.session.user.id,
+        depth: 0,   // user.tenants[0].tenant is going to be a string (tenant ID);
+     });
+
+     if (!user) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "User not found",
+      });
+     };
+
+     const tenantId = user.tenants?.[0]?.tenant as string;  // This is an id because of depth: 0
+     const tenant = await ctx.db.findByID({
+      collection: "tenants",
+      id: tenantId,
+     });
+
+     if (!tenant) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Tenant not found",
+      });
+     };
+
+     const accountLink = await stripe.accountLinks.create({
+      account: tenant.stripeAccountId,
+      refresh_url: `${process.env.NEXT_PUBLIC_APP_URL!}/admin`,
+      return_url: `${process.env.NEXT_PUBLIC_APP_URL!}/admin`,
+      type: "account_onboarding",
+     });
+
+     if (!accountLink.url) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Failed to create verification link"
+      })
+     }
+
+     return { url: accountLink.url };
+    }),
   purchase: protectedProcedure
     .input(
       z.object({
